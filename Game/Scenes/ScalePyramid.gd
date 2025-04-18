f54ebe581c17aef8730430a2b9732fda0656cb70
jw-xplore <jwernerltug@gmail.com>
2025-04-18 18:13:21 +0200

    Removed physical progression scripts

--- Game/Scenes/ScalePyramid.gd
+++
@@ -1,9 +0,0 @@
-extends StaticBody2D
-class_name ScalePyramid
-
-@export var scale_size: float = 64
-@export var start_point: Node2D
-@export var collision: CollisionPolygon2D
-
-func enable_collisions(enable: bool):
-	collision.disabled = !enable
